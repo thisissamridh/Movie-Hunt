5b97d01cd2bfdc9e776c629c70a7f1e5c4fe2fe9
thisissamridh <samridhsinghind@gmail.com>
2022-07-28 16:45:16 +0530

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,56 @@
-import React, { useEffect } from "react";
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import MovieCard from "./MovieCard";
+import searchIcon from "./search.svg";
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=ce110aaa";
 
 const App = () => {
-  const searchMovies = async (title) => {
+  const [movies, setMovies] = React.useState([]);
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const searchmovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data);
+    setMovies(data.Search);
   };
+
   useEffect(() => {
-    searchMovies("ajab prem ki gazab kahani");
+    searchmovies("avengers");
   }, []);
-  return <div>App</div>;
+
+  return (
+    <div className="app">
+      <h1>Movie Land</h1>
+
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for a movie"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && searchmovies(searchTerm)}
+        />
+
+        <img
+          src={searchIcon}
+          alt="search"
+          onClick={() => searchmovies(searchTerm)}
+        ></img>
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => {
+            return <MovieCard movies={movie} />;
+          })}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
---
+++ src/MovieCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const MovieCard = ({ movies }) => {
+  return (
+    <div className="movie">
+      <div>
+        <p>{movies.Year}</p>
+      </div>
+      <div>
+        <img
+          src={
+            movies.Poster !== "N/A"
+              ? movies.Poster
+              : "https://via.placeholder.com/400"
+          }
+          alt=""
+        />
+      </div>
+      <div>
+        <span>{movies.Type}</span>
+        <h3>{movies.Title}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
